c894bbc048ae3ad81d217dfc6da82c06bf22a2c5
vignezds <vignezds@gmail.com>
2017-06-20 19:06:36 +0530

    Create vowelorcons.cpp

---
+++ vowelorcons.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char alp;
+  int lower, upper;
+  cout<<"Enter alphabet";
+  cin>>alp;
+  lower = (apl == 'a' ||apl == 'e' ||apl == 'i' ||apl == 'o' ||apl == 'u');
+  upper = (apl == 'A' ||apl == 'E' ||apl == 'I' ||apl == 'O ||apl == 'U');
+  if(lower||upper)
+  cout<<"Vowels";
+  else
+  cout<<"consonant";
+  return 0;
+}
+  
