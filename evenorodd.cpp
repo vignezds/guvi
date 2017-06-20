af9fce818ddd9d0c0b5bb6f68241545b411c471c
vignezds <vignezds@gmail.com>
2017-06-20 14:18:11 +0530

    Create evenorodd.cpp

---
+++ evenorodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+  int n;
+  cout<<"Enter number";
+  cin>>n;
+  if(n%2==0)
+  cout<<"Entered number is even";
+  else
+  cout<<"Entered number is odd";
+  return 0;
+}
