69d88490bb697bff78554677e63b4beb5097b425
vignezds <vignezds@gmail.com>
2017-06-21 09:32:02 +0530

    Create largestof3no.cpp

---
+++ largestof3no.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n1,n2,n3;
+  cout<<"Enter three numbers";
+  cin>>n1>>n2>>n3;
+  if(n1>n2 && n1>n3)
+  cout<<n1<<"is the largest number";
+  else if(n2>n1 && n2>n3)
+  cout<<n2<<"is the largest number";
+  else
+  cout<<n3<<"is the largest number";
+  return 0;
+}
