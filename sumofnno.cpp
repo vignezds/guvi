093682b97af4f7e76b7889075ff140a83af4040f
vignezds <vignezds@gmail.com>
2017-06-24 14:43:32 +0530

    Create sumofnno.cpp

---
+++ sumofnno.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  int sum = 0;
+  cout<<"Enter any number";
+  cin>>n;
+  for(int i = 0 ; i < = n ; i++)
+  {
+    sum = sum + i;
+  }
+  cout<<"Sum of"<<n<<"is"<<sum;
+  return 0;
+}
