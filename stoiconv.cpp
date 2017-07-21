7cc8f44036d5b443aa26879004d1940cd1616d13
vignezds <vignezds@gmail.com>
2017-07-21 10:09:18 +0530

    Create stoiconv.cpp

---
+++ stoiconv.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void main()
+{
+    string i = "98765";
+    string f = "3.14";
+    int i = stoi(i);
+    float f = stof(f);
+    cout << i << " " << f <<endl;
+}
